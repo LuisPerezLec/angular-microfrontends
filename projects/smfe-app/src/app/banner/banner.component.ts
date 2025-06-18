6a328a92501e9431bf25d10856901703b973d518
LuisPerezLec <93443241+LuisPerezLec@users.noreply.github.com>
2025-06-18 00:18:41 -0600

    Sending events

--- projects/smfe-app/src/app/banner/banner.component.ts
+++ projects/smfe-app/src/app/banner/banner.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   imports: [],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+  styleUrl: './banner.component.scss',
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit, OnDestroy {
+  message = 'Esperando evento...';
 
+  private handler = (event: Event) => {
+    const customEvent = event as CustomEvent;
+    this.message = 'Evento recibido: ' + customEvent.detail.message;
+  };
+
+  ngOnInit(): void {
+    window.addEventListener('login-clicked', this.handler);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('login-clicked', this.handler);
+  }
 }
